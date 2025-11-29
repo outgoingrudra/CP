aae0dc4e2a9553819805be65564560d36033b174
Rudra Verma <rudraverma781@gmail.com>
2025-11-29 17:49:09 +0530

    Dim Light

---
+++ leetcode/q3512.java
@@ -0,0 +1,14 @@
+// You are given an integer array nums and an integer k. You can perform the following operation any number of times:
+
+// Select an index i and replace nums[i] with nums[i] - 1.
+// Return the minimum number of operations required to make the sum of the array divisible by k.
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int s = 0;
+        for(int i : nums)
+             s+= i;
+        return s%k ;
+        
+    }
+}
